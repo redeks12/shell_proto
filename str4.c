0a5de57be5087efb2a03bf0755325f433334e821
kumdan job <redeks123456@gmail.com>
2023-05-20 13:34:21 +0100

    okay

--- str4.c
+++ str4.c
@@ -91,12 +91,12 @@ int matchStringsAndDelim(char *string1, char *string2, char *delimiter)
 
     while (string1[index] == string2[index])
     {
-        if (matchStringsAndDelim(string1[index], delimiter))
+        if (characterMatch(string1[index], delimiter))
             return (1);
         index++;
     }
 
-    if (isDelimiter(string1[index], delimiter) || isDelimiter(string2[index], delimiter))
+    if (characterMatch(string1[index], delimiter) || characterMatch(string2[index], delimiter))
         return (1);
 
     return (0);
